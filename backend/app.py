6f1e3ebb7a5156d53fedfc553903b1c8855bc4fb
plucodev <paololucanodeveloper@gmail.com>
2019-04-02 09:07:23 -0400

    Setting up frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, jsonify
+  
+app = Flask(__name__)
+  
+@app.route('/')
+def hello():
+    return jsonify({ "hello": "World" })
+  
+  
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
