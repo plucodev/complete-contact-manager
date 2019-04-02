0aa618e31869d6116be55b05dfeb49782eec11a7
plucodev <paololucanodeveloper@gmail.com>
2019-04-02 14:23:46 -0400

    port changed in flask

--- backend/app.py
+++ backend/app.py
@@ -8,4 +8,4 @@ def hello():
     return jsonify({ "hello": "World" })
   
   
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8000)))
